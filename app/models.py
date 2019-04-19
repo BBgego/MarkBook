850d5a12c6d4c2c7ce02775ca648132f319129dd
gyw <1299622716@qq.com>
2019-04-19 21:13:46 +0800

    鸡你太美
    Signed-off-by: gyw <1299622716@qq.com>

--- app/models.py
+++ app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 用户表
+class User(models.Model):
+    # 用户sid
+    sid = models.CharField(max_length=20, unique=True)
+    # 用户名
+    name = models.CharField(max_length=50)
+    # 用户性别  0：女 1：男
+    sex = models.IntegerField(default=1)
+    # 手机号
+    tel = models.CharField(max_length=20, unique=True)
+    # 邮箱
+    email = models.CharField(max_length=20, unique=True, null=True)
+    # 密码
+    password = models.CharField(max_length=100)
+    # 创建时间
+    create_time = models.CharField(max_length=100)
+    # 是否删除
+    is_del = models.BooleanField(default=False)
+
+
+# 文集
+class Corpus(models.Model):
+    # 文集名
+    name = models.CharField(max_length=50)
+    # 文件夹
+    dir_name = models.CharField(max_length=50)
+    # 用户关联
+    user = models.ForeignKey(User, on_delete="PROTECT")
+    # 创建时间
+    create_time = models.CharField(max_length=50)
+    # 是否删除
+    is_del = models.BooleanField(default=False)
+
+
+# 文章
+class Book(models.Model):
+    # 文章名
+    name = models.CharField(max_length=50)
+    # 文件
+    file_name = models.CharField(max_length=50)
+    # 文集关联
+    corpus = models.ForeignKey(Corpus, on_delete="PROTECT")
+    # 创建时间
+    create_time = models.CharField(max_length=50)
+    # 是否删除
+    is_del = models.BooleanField(default=False)
