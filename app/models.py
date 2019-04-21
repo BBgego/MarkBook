ccc88563d792d954c30b101bbb8b7f80192e839a
gyw <1299622716@qq.com>
2019-04-21 09:41:49 +0800

    鸡你太美
    Signed-off-by: gyw <1299622716@qq.com>

--- app/models.py
+++ app/models.py
@@ -49,5 +49,37 @@ class Book(models.Model):
     corpus = models.ForeignKey(Corpus, on_delete="PROTECT")
     # 创建时间
     create_time = models.CharField(max_length=50)
+    # 喜欢数
+    love = models.IntegerField(default=0)
+    # 浏览数
+    look = models.IntegerField(default=0)
+    # 是否删除
+    is_del = models.BooleanField(default=False)
+
+
+# 父级评论
+class FatherArt(models.Model):
+    # 评论内容
+    content = models.CharField(max_length=200)
+    # 评论用户
+    user = models.ForeignKey(User, on_delete="PROTECT")
+    # 评论文章
+    book = models.ForeignKey(Book, on_delete="PROTECT")
+    # 评论时间
+    art_time = models.CharField(max_length=20)
+    # 是否删除
+    is_del = models.BooleanField(default=False)
+
+
+# 子级评论
+class SonArt(models.Model):
+    # 评论内容
+    content = models.CharField(max_length=200)
+    # 评论用户
+    user = models.ForeignKey(User, on_delete='PROTECT')
+    # 回复用户
+    send_user_name = models.CharField(max_length=50)
+    # 评论时间
+    art_time = models.CharField(max_length=20)
     # 是否删除
     is_del = models.BooleanField(default=False)
